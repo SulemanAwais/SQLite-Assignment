156003857e51d5a196a62c0c5d4b58a283497008
Suleman Awais <bsef19a041@pucit.edu.pk>
2022-09-07 06:17:02 +0500

    Home Page View

---
+++ app/src/main/java/com/example/sqlitecrudapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sqlitecrudapp;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class MainActivity extends  AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
