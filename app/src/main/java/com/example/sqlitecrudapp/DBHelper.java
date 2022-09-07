21de8e03061a17999d14a127d842eb98ff5c36a0
Suleman Awais <bsef19a041@pucit.edu.pk>
2022-09-07 22:00:31 +0500

    View Students

--- app/src/main/java/com/example/sqlitecrudapp/DBHelper.java
+++ app/src/main/java/com/example/sqlitecrudapp/DBHelper.java
@@ -2,6 +2,7 @@ package com.example.sqlitecrudapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,21 +16,20 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String createTableStatement = "CREATE TABLE STUDENT_TABLE"+
-        "( StudentID Integer PRIMARY KEY AUTOINCREMENT, STUDENT_NAME Text,STUDENT_ROLLNUM Text ,ENROLLED_STUDENT BOOL)" ;
+        "( StudentID Integer PRIMARY KEY AUTOINCREMENT, STUDENT_NAME Text,STUDENT_ROLLNUM Text)" ;
         sqLiteDatabase.execSQL(createTableStatement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("drop table if exists StudentDetails");
+        sqLiteDatabase.execSQL("drop table if exists STUDENT_TABLE");
     }
-    public Boolean AddStudent(String name, String rollNum, Boolean status){
+    public Boolean AddStudent(String name, String rollNum){
         SQLiteDatabase db=this.getWritableDatabase();
         ContentValues values=new ContentValues();
         values.put("STUDENT_NAME",name);
         values.put("STUDENT_ROLLNUM",rollNum);
-        values.put("ENROLLED_STUDENT",status);
-        long insert =db.insert("StudentDetails",null,values);
+        long insert =db.insert("STUDENT_TABLE",null,values);
         if (insert==-1)
         {
             return false;
@@ -44,7 +44,7 @@ public class DBHelper extends SQLiteOpenHelper {
         values.put("STUDENT_NAME",name);
         values.put("STUDENT_ROLLNUM",rollNum);
         values.put("ENROLLED_STUDENT",status);
-        long insert =db.insert("StudentDetails",null,values);
+        long insert =db.insert("STUDENT_TABLE",null,values);
         if (insert==-1)
         {
             return false;
@@ -53,4 +53,11 @@ public class DBHelper extends SQLiteOpenHelper {
             return true;
         }
     }
+    public Cursor getData()
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor c=db.rawQuery("SELECT * from STUDENT_TABLE",null);
+        return c;
+    }
+
 }
--- app/src/main/java/com/example/sqlitecrudapp/MainActivity.java
+++ app/src/main/java/com/example/sqlitecrudapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sqlitecrudapp;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
@@ -13,6 +16,7 @@ public class MainActivity extends  AppCompatActivity{
     EditText name, rollNum;
     Switch status;
     Button addStudentBtn;
+    Button viewStudents;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,23 +25,47 @@ public class MainActivity extends  AppCompatActivity{
 
         name=findViewById(R.id.studentName);
         rollNum=findViewById(R.id.studentRollNum);
-        status=findViewById(R.id.IsEnrolled);
 
-        addStudentBtn=findViewById(R.id.viewList);
+        viewStudents=findViewById(R.id.viewList);
+        addStudentBtn=findViewById(R.id.AddButton);
         DBHelper db =new DBHelper(this);
         addStudentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String Student_name=name.getText().toString();
                 String Student_RollNum=rollNum.getText().toString();
-                boolean Student_status=status.getShowText();
-                boolean x=true;
-                Boolean isAdded =db.AddStudent(Student_name,Student_RollNum,x);
+                Boolean isAdded =db.AddStudent(Student_name,Student_RollNum);
                 if (isAdded==true)
                 {
                     Toast.makeText(getApplicationContext(),"NEW STUDENT ADDED",Toast.LENGTH_LONG).show();
                 }
-                
+
+            }
+        });
+        viewStudents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor c=db.getData();
+            if (c.getCount()==0)
+            {
+                Toast.makeText(getApplicationContext(),"NO DATA FOUND",Toast.LENGTH_LONG).show();
+                return;
+            }
+            else
+            {
+                StringBuffer stringBuffer=new StringBuffer();
+                while(c.moveToNext())
+                {
+                    stringBuffer.append("STUDENT_NAME :"+c.getString(1)+"\n");
+                    stringBuffer.append("STUDENT_ROLLNUM :"+c.getString(2)+"\n");
+                    
+                }
+                AlertDialog.Builder alertBuilder=new AlertDialog.Builder(MainActivity.this);
+                alertBuilder.setCancelable(true);
+                alertBuilder.setTitle("STUDENT LIST");
+                alertBuilder.setMessage(stringBuffer.toString());
+                alertBuilder.show();
+            }
             }
         });
 
